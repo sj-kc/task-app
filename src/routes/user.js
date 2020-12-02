79f458802465be079c2afe937223562f58827dc4
sungkimcastro <s.kim.castro@gmail.com>
2020-12-02 20:07:14 +0200

    Read profile, logout, logout all

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 // Create User
 router.post('/users', async (req, res) => {
-  const user = new User(req.body);
   try {
+    const user = new User(req.body);
     await user.save();
     const token = await user.generateToken();
 
-    res.send({ user, token });
+    res.status(201).send({ user, token });
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send();
   }
 });
 
@@ -26,5 +27,36 @@ router.post('/users/login', async (req, res) => {
     res.status(400).send(message);
   }
 });
+
+// Logout
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(
+      ({ token }) => token !== req.token
+    );
+
+    await req.user.save();
+
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//Logout All
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+// Read Profile
+router.get('/users/me', auth, (req, res) => res.send(req.user));
 
 module.exports = router;
