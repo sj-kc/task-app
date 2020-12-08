b456eb0bcabc3067a3cd69010aeedb93579afedd
sungkimcastro <s.kim.castro@gmail.com>
2020-12-08 20:56:32 +0200

    Get Task

--- src/routes/task.js
+++ src/routes/task.js
@@ -17,4 +17,16 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+router.get('/tasks/:id', auth, async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findOne({ _id, owner: req.user._id });
+    if (!task) return res.status(404).send();
+    res.send({ task });
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
