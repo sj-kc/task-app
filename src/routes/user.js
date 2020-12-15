6cc1f6f4b8bce1f5914958fc783dce82daaa224a
sungkimcastro <s.kim.castro@gmail.com>
2020-12-15 20:12:55 +0200

    Send email for creating account, removing account

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
 const auth = require('../middleware/auth');
+const { welcome, cancel } = require('../emails/account');
 
 router.post('/users', async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
+    welcome(user);
     const token = await user.generateToken();
-
     res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send();
@@ -71,7 +72,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
   try {
     await req.user.remove();
-
+    cancel(req.user);
     res.send(req.user);
   } catch (error) {
     res.status(500).send;
